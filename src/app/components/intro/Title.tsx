4285acae712dec59441719f2e2ca4f8ae2c98395
Uichan Lee <cksxkr5193@naver.com>
2024-01-19 23:59:19 +0900

    Feat: Title 컴포넌트 구현

---
+++ src/app/components/intro/Title.tsx
@@ -0,0 +1,12 @@
+export default function Title() {
+  return (
+    <h1 className="title my-10">
+      가치를 만드는
+      <br />
+      FE 개발자
+      <br />
+      이의찬입니다
+      <span className="animate-blink text-blue-400">.</span>
+    </h1>
+  );
+}
