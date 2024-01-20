cede6a088554fd11bc8eec1b241021f271f0e4df
Legitgoons <cksxkr5193@naver.com>
2024-01-21 00:06:21 +0900

    Chore: 전체 너비와 Title image margin 수정

--- src/app/components/intro/Title.tsx
+++ src/app/components/intro/Title.tsx
@@ -16,7 +16,7 @@ export default function Title() {
       <Image
         src={photo}
         alt="photo"
-        className="ml-6 w-52 self-center md:ml-10 lg:ml-20"
+        className="ml-6 w-52 self-center md:ml-10 lg:ml-16"
       />
     </section>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Education from './components/profile/Education';
 
 export default function Home() {
   return (
-    <main className="flex w-4/5 flex-col gap-y-10 break-keep md:w-2/3 lg:w-7/12">
+    <main className="flex w-4/5 flex-col gap-y-10 break-keep md:w-2/3 lg:w-1/2 xl:w-2/5">
       <Title />
       <Intro />
       <Education />
