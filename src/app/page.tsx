02829a22641047f766249321388f2347811c6c31
Uichan Lee <cksxkr5193@naver.com>
2024-01-20 16:11:21 +0900

    Feat: Home 컴포넌트 profile 항목 추가 및 디자인 요소 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Intro from './components/intro/Intro';
 import Title from './components/intro/Title';
+import Contact from './components/profile/Contact';
+import Education from './components/profile/Education';
 
 export default function Home() {
   return (
-    <main className="w-4/5 gap-y-4 md:w-2/3 lg:w-3/5">
+    <main className="flex w-4/5 flex-col gap-y-10 md:w-2/3 lg:w-3/5 ">
       <Title />
       <Intro />
+      <Education />
+      <Contact />
     </main>
   );
 }
