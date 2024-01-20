30a988cc892b9c1b512e8846f11d0199558b9892
Uichan Lee <cksxkr5193@naver.com>
2024-01-20 14:14:43 +0900

    Feat: Title 컴포넌트에 사진 추가 및 반응형 디자인 적용

--- src/app/components/intro/Title.tsx
+++ src/app/components/intro/Title.tsx
@@ -1,12 +1,23 @@
+import Image from 'next/image';
+
+import photo from '@/../public/assets/img/photo.webp';
+
 export default function Title() {
   return (
-    <h1 className="title my-10">
-      가치를 만드는
-      <br />
-      FE 개발자
-      <br />
-      이의찬입니다
-      <span className="animate-blink text-blue-400">.</span>
-    </h1>
+    <section className="flex flex-wrap-reverse">
+      <h1 className="t1 my-10">
+        가치를 만드는
+        <br />
+        FE 개발자
+        <br />
+        이의찬입니다
+        <span className="animate-blink text-blue-400">.</span>
+      </h1>
+      <Image
+        src={photo}
+        alt="photo"
+        className="ml-6 w-52 self-center sm:ml-12"
+      />
+    </section>
   );
 }
