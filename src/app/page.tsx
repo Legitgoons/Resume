912bdbdbcd6226dc983c52a630da089993a97c7c
Uichan Lee <cksxkr5193@naver.com>
2024-01-28 23:43:08 +0900

    Feat: page.tsx Home 컴포넌트 Project 추가 및 반응형 사이즈 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
+import { projectData } from '@/app/data/project';
+
 import Intro from './components/intro/Intro';
 import Title from './components/intro/Title';
+import ItemTitle from './components/ItemTitle';
 import Contact from './components/profile/Contact';
 import Education from './components/profile/Education';
+import Project from './components/project/Project';
 
 export default function Home() {
   return (
-    <main className="flex w-4/5 max-w-3xl flex-col gap-y-10 break-keep md:w-2/3 lg:w-1/2 2xl:w-2/5">
+    <main className="my-12 flex w-4/5 max-w-3xl flex-col gap-y-12 break-keep md:w-2/3 lg:w-1/2 2xl:w-2/5">
       <Title />
       <Intro />
       <Education />
       <Contact />
+      <ItemTitle>Project</ItemTitle>
+      <Project projectData={projectData} />
     </main>
   );
 }
