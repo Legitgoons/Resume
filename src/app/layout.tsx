50dfbc33d3a2fe607c78e1164bcfa088419a3c98
Uichan Lee <cksxkr5193@naver.com>
2024-02-29 17:17:19 +0900

    Feat: Layout.tsx에 SEO를 위한 metadata 추가 작성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,61 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: '가치를 만드는 FE 개발자 이의찬입니다',
   description: '신입 프론트엔드 개발자 이의찬의 웹 포트폴리오 사이트입니다.',
+  generator: 'Next.js',
+  applicationName: 'Frontend-Portfolio',
+  keywords: [
+    'Frontend',
+    'Frontend developer',
+    '프론트엔드',
+    '프론트엔드 채용',
+    'Next.js',
+    'React',
+    'JavaScript',
+    'TypeScript',
+  ],
+  category: 'Portfolio',
+  metadataBase: new URL('https://web-portfolio-legitgoons.vercel.app'),
+  authors: { name: '이의찬', url: 'https://github.com/Legitgoons' },
+  creator: '이의찬',
+  publisher: '이의찬',
+  formatDetection: {
+    email: true,
+    telephone: true,
+  },
+  openGraph: {
+    title: '가치를 만드는 FE 개발자 이의찬입니다',
+    description: '신입 프론트엔드 개발자 이의찬의 웹 포트폴리오 사이트입니다.',
+    url: 'https://web-portfolio-legitgoons.vercel.app',
+    siteName: 'Frontend-Portfolio',
+    images: [
+      {
+        url: 'https://i.ibb.co/TL6q9vW/Kakao-Talk-20230522-210754586.png',
+        width: 583,
+        height: 750,
+        alt: 'Profile picture',
+      },
+    ],
+    locale: 'ko_KR',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  verification: {
+    google: 'Ay2fKJa1Zp8gBgeHh9jSDr3JboRNy8x0h8niXS9jNhQ',
+    other: {
+      'naver-site-verification': '4bbc96fa91d151689912e4998ffe04ffba5c4813',
+    },
+  },
 };
 
 export default function RootLayout({
