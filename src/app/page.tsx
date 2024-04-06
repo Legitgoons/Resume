c45ad201c5a506296dcbce5ae220f28649adc6a0
Uichan Lee <cksxkr5193@naver.com>
2024-04-06 17:31:42 +0900

    Feat: Home 컴포넌트에 ThemeProvider 적용, contactData Props로 전달 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import { Analytics } from '@vercel/analytics/react';
+
 import { connectDB } from '@/app/util/data';
 
+import Progressbar from './components/app/Progressbar';
+import ThemeButton from './components/app/ThemeButton';
 import Experience from './components/experience/Experience';
 import Intro from './components/intro/Intro';
 import Title from './components/intro/Title';
@@ -7,12 +11,17 @@ import Contact from './components/profile/Contact';
 import Education from './components/profile/Education';
 import OtherProject from './components/project/OtherProject';
 import Project from './components/project/Project';
+import ThemeProvider from './components/provider/ThemeProvider';
 import ItemTitle from './components/shared/ItemTitle';
-import { contactData } from './data.ts/contactData';
+import { ContactResponse } from './type/contact';
 import { ExperienceResponse } from './type/experience';
 import { ProjectDefaultResponse, ProjectResponse } from './type/project';
 
 const db = (await connectDB).db('Portfolio');
+const contact = await db
+  .collection<ContactResponse>('Contact')
+  .find()
+  .toArray();
 const exp = await db.collection<ExperienceResponse>('Exp').find().toArray();
 const projects = await db
   .collection<ProjectResponse>('Projects')
@@ -25,22 +34,27 @@ const otherProjects = await db
 
 export default function Home() {
   return (
-    <main className="my-12 flex w-4/5 max-w-3xl flex-col gap-y-12 break-keep lg:w-2/3 2xl:w-1/2">
-      <Title />
-      <Intro />
-      <Education />
-      <Contact contactData={contactData} />
-      <hr />
-      <ItemTitle>Projects</ItemTitle>
-      <div className="flex flex-col gap-y-12">
-        <Project projectData={projects} />
+    <ThemeProvider enableSystem>
+      <Progressbar />
+      <ThemeButton />
+      <main className="my-12 flex w-4/5 max-w-3xl flex-col gap-y-12 break-keep lg:w-2/3 2xl:w-1/2">
+        <Title />
+        <Intro />
+        <Education />
+        <Contact contactData={contact} />
         <hr />
-        <ItemTitle>Other Projects</ItemTitle>
-      </div>
-      <OtherProject projectData={otherProjects} />
-      <hr />
-      <ItemTitle>Experience</ItemTitle>
-      <Experience expData={exp} />
-    </main>
+        <ItemTitle>Projects</ItemTitle>
+        <div className="flex flex-col gap-y-12">
+          <Project projectData={projects} />
+          <hr />
+          <ItemTitle>Other Projects</ItemTitle>
+        </div>
+        <OtherProject projectData={otherProjects} />
+        <hr />
+        <ItemTitle>Experience</ItemTitle>
+        <Experience expData={exp} />
+      </main>
+      <Analytics />
+    </ThemeProvider>
   );
 }
