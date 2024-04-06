92fd3ad62f5dfc221111fbfcad84bb444cfd307f
Uichan Lee <cksxkr5193@naver.com>
2024-04-06 17:28:48 +0900

    Feat: github icon을 변경하며 contact data를 mongoDB에서 가져오도록 수정

--- src/app/type/contact.ts
+++ src/app/type/contact.ts
@@ -1,11 +1,17 @@
+import { WithId, Document } from 'mongodb';
+
 interface ImageProps {
   src: string;
   alt: string;
 }
 
-export interface ContactItem {
+interface ContactItem {
   icon: string;
   link: string;
   text: string;
   image?: ImageProps;
+}
+
+export interface ContactResponse extends WithId<Document> {
+  data: ContactItem[];
 }
