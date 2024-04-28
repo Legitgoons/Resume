3ab3c401aa74f2a57fdf8f778f6b2d1cc94bccb9
이의찬 <cksxkr5193@naver.com>
2024-04-28 18:55:57 +0900

    study  / others 항목 추가, fetchData 분리 (#24)
    * feat: Others 컴포넌트 생성 및 Experience 컴포넌트 others 폴더로 이동
    
    * feat: others.ts로 type/experience.ts 이름 수정
    
    * feat: Other 추가 및 Education 컴포넌트 삭제
    
    * feat: study 항목 추가, fetchData 분리
    
    * feat: fetchData에서 가져온 data들 import해서 사용하도록 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
 
-import { connectDB } from '@/app/util/data';
-
 import Progressbar from './components/app/Progressbar';
 import ThemeButton from './components/app/ThemeButton';
 import Intro from './components/intro/Intro';
@@ -13,32 +11,21 @@ import OtherProject from './components/project/OtherProject';
 import Project from './components/project/Project';
 import ThemeProvider from './components/provider/ThemeProvider';
 import ItemTitle from './components/shared/ItemTitle';
-import { ContactResponse } from './type/contact';
-import { OthersResponse, ExperienceResponse } from './type/others';
-import { ProjectDefaultResponse, ProjectResponse } from './type/project';
-
-const db = (await connectDB).db('Portfolio');
-const contact = await db
-  .collection<ContactResponse>('Contact')
-  .find()
-  .toArray();
-const exp = await db.collection<ExperienceResponse>('Exp').find().toArray();
-const others = await db.collection<OthersResponse>('Others').find().toArray();
-const projects = await db
-  .collection<ProjectResponse>('Projects')
-  .find()
-  .toArray();
-const otherProjects = await db
-  .collection<ProjectDefaultResponse>('otherProjects')
-  .find()
-  .toArray();
+import {
+  contact,
+  exp,
+  otherProjects,
+  others,
+  projects,
+  study,
+} from './util/fetchData';
 
 export default function Home() {
   return (
     <ThemeProvider enableSystem>
       <Progressbar />
       <ThemeButton />
-      <main className="my-12 flex w-4/5 max-w-3xl flex-col gap-y-12 break-keep lg:w-2/3 2xl:w-1/2">
+      <main className="my-12 flex w-4/5 max-w-3xl flex-col gap-y-8 break-keep lg:w-2/3 2xl:w-1/2">
         <Title />
         <Intro />
         <Contact contactData={contact} />
@@ -51,6 +38,8 @@ export default function Home() {
         </div>
         <OtherProject projectData={otherProjects} />
         <hr />
+        <ItemTitle>Study</ItemTitle>
+        <Others othersData={study} />
         <ItemTitle>Experience</ItemTitle>
         <Experience expData={exp} />
         <ItemTitle>Others</ItemTitle>
